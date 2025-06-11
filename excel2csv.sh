8d9496fc91347443c7d6116c8f2cb734772133c8
jac <me@jorge.red>
2025-06-11 23:59:55 +0200

    changes

--- excel2csv.sh
+++ excel2csv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat ultima_version.csv |awk -F';' '{print $2 ";" $4 ";" $1}' >> inscritos_$(date +%Y).csv
+cat ultima_version.csv |awk -F';' '{print $1 ";" $3}' >> inscritos_$(date +%Y).csv
